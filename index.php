488e91d692efdc3f7a11e84a64471c98beae6afc
z3renato <zerenato.souza@bcc.unifal-mg.edu.br>
2014-12-05 13:25:20 -0200

    commit inicial

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Jogo</title>
+        <link href="css/stylesheet.css" type="text/css" rel="stylesheet">
+        <script src="js/js.js" type="text/javascript" ></script>
+    </head>
+    <body >
+        
+        corpo
+        <div id="quadro">
+            <div id="img-fundo">
+                <img src="img/light-novel-quadrinize.jpg">
+            </div>
+            <div id="dialogo">
+                <h1 id="texto-dialogo">Texto 1</h1>
+            </div>
+            <button onclick="altera()">Dê alegria para o Daniel</button>
+
+        </div>
+
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
