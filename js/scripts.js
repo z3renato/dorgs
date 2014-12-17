0cc52ed2dd4d0024eaa6bb04cc8d453508f9ceb7
z3renato <zerenato.souza@bcc.unifal-mg.edu.br>
2014-12-17 05:14:04 -0200

    Apenas uma opção pode ser escolhida!
    Também foi capturado a escolha

--- js/scripts.js
+++ js/scripts.js
@@ -3,37 +3,66 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+escolha="";
 
+function setEscolha(escolha){
+    this.escolha = escolha;
+}
 function altera() {
-    document.getElementById("texto-dialogo").innerHTML="NOVO TEXTO";
+    document.getElementById("texto-dialogo").innerHTML = "NOVO TEXTO";
 }
 
 //Função de Drag&Drop
 function allowDrop(ev) {
+
     ev.preventDefault();
+
 }
 
 function drag(ev) {
-    ev.dataTransfer.setData("Text", ev.target.id);
-}
-function getConteudo(){
-    var qtd = $("#op > span").size();
-    
-    alert(qtd);
-}
 
+
+    ev.dataTransfer.setData("Text", ev.target.id);
+
+}
+function getConteudo() {
+    var qtd = $("#op > span").contents().size();
+
+   
+    return qtd;
+}
+function verifica(proxima) {
+
+    if (getConteudo() == 0) {
+        alert("Selecione uma opção!!!");
+
+    } else {
+        
+        alert(escolha);
+        window.location = proxima;
+    }
+}
+function allowDropOp(ev) {
+    if (getConteudo() == 0) {
+         ev.preventDefault();
+      
+    }
+}
 function drop(ev) {
+
     var data = ev.dataTransfer.getData("Text");
     ev.target.appendChild(document.getElementById(data));
     ev.preventDefault();
-//    
+   
+
+//    }
 //    document.getElementById("op-4").id="juntos";
 //    document.getElementById("separados").draggable = false;
 //    document.getElementById("morto").draggable = false;
 //    document.getElementById("outro").draggable = false;
-  
+
 }
 // /Função de Drag&Drop
-function exibe(texto){
+function exibe(texto) {
     window.alert(texto);
 }
--- pergunta1.php
+++ pergunta1.php
@@ -9,16 +9,16 @@
 <div id="dialogo">
     <h2 id="texto-dialogo" class='text-center'>Qual seu sexo?</h2>
     <div class='row'>
-        <div class='opcao col-xs-2 col-xs-offset-5' ondrop="drop(event)" ondragover="allowDrop(event)" id='op'></div>
+        <div class='opcao col-xs-2 col-xs-offset-5'  ondrop="drop(event)" ondragover="allowDropOp(event)" id='op'></div>
     </div>
     <div class='row' id='escolha-group'>
-        <div class='escolha-holder col-xs-2 col-xs-offset-2' ondrop="drop(event)" ondragover="allowDrop(event)" id='escolha-homem'>
-            <span class='escolha col-xs-2 col-xs-offset-1 text-center' id='homem' draggable="true" ondragstart="drag(event)">♂</span>
+        <div class='escolha-holder col-xs-2 col-xs-offset-2' ondrop="drop(event)" ondragover="allowDrop(event)" id='escolha'>
+            <span class='escolha col-xs-2 col-xs-offset-1 text-center' id="homem" draggable="true" ondragstart="drag(event)" ondragend="setEscolha('homem')">♂</span>
         </div>
-        <div class='escolha-holder col-xs-2 col-xs-offset-4' ondrop="drop(event)" ondragover="allowDrop(event)" id='escolha-mulher'>
-            <span class='escolha col-xs-2   text-center' id='mulher' draggable="true" ondragstart="drag(event)">♀</span>
+        <div class='escolha-holder col-xs-2 col-xs-offset-4' ondrop="drop(event)" ondragover="allowDrop(event)" id='escolha'>
+            <span class='escolha col-xs-2   text-center' id='mulher' draggable="true" ondragstart="drag(event)"ondragend="setEscolha('mulher')">♀</span>
         </div>
     </div>
-    <a  class="btn btn-primary btn-lg col-xs-2 col-xs-offset-5" id="enviar"onclick="getConteudo()" href="pergunta2.php"  >Próxima</a>
+    <a  class="btn btn-primary btn-lg col-xs-2 col-xs-offset-5" id="enviar"onclick="verifica('pergunta2.php')"  >Próxima</a>
 </div>
 <?php require 'includes/js.php'; ?>
