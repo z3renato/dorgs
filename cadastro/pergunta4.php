bf84276bee10c3188c514e00495d7c172d5bf5f8
z3renato <zerenato.souza@bcc.unifal-mg.edu.br>
2015-02-04 12:46:39 -0200

    4

---
+++ cadastro/pergunta4.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+echo "<head>
+    <meta charset='UTF-8'>
+    <title>Jogo</title>
+    <?php require 'includes/css.php'; ?>
+</head>";
+
